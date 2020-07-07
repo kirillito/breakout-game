ae12b469d3aded3348b3428b8757104b5e6d1bb3
ANACA\kirill.bredikhin <kirillb@xello.world>
2020-07-06 23:00:05 -0400

    Level from array, wip

--- src/brick-level.js
+++ src/brick-level.js
@@ -2,21 +2,91 @@ const BRICK_W = 80;
 const BRICK_H = 20;
 const BRICK_GAP = 2;
 const BRICK_COLS = 10;
-const BRICK_ROWS = 14;
+const BRICK_ROWS = 18;
 
 const BRICK_SCORE = 10;		
 
 class BrickLevel {
+  LEVEL3_GRID = [
+    0,0,0,0,0,0,0,0,0,0,
+    0,1,0,0,1,1,0,0,1,0,
+    0,0,0,0,1,1,0,0,0,0,
+    0,1,0,0,1,1,0,0,1,0,
+    1,1,1,1,0,0,1,1,1,1,
+    1,0,1,1,1,1,1,1,0,1,
+    0,1,1,1,0,0,1,1,1,0,
+    1,0,1,1,1,1,1,1,0,1,
+    1,1,1,1,0,0,1,1,1,1,
+    1,0,1,1,1,1,1,1,0,1,
+    1,1,1,1,1,1,1,1,1,1,
+    1,0,1,0,0,0,0,1,0,1,
+    0,1,1,1,1,1,1,1,1,0,
+    1,0,1,0,0,0,0,1,0,1,
+    0,1,1,1,1,1,1,1,1,0,
+    1,0,1,0,0,0,0,1,0,1,
+    0,1,1,1,1,1,1,1,1,0,
+    0,0,1,1,0,0,1,1,0,0
+  ];
+
+  LEVEL1_GRID = [
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1
+  ];
+
+  LEVEL2_GRID = 
+  [
+    0,0,0,0,0,0,0,0,0,0,
+    0,1,1,0,1,1,0,1,1,0,
+    0,1,1,0,1,1,0,1,1,0,
+    0,1,1,1,1,1,1,1,1,0,
+    0,1,1,1,1,1,1,1,1,0,
+    0,1,1,0,1,1,0,1,1,0,
+    0,1,1,0,1,1,0,1,1,0,
+    0,1,1,1,1,1,1,1,1,0,
+    0,1,1,1,1,1,1,1,1,0,
+    0,1,1,0,1,1,0,1,1,0,
+    0,1,1,0,1,1,0,1,1,0,
+    0,1,1,1,1,1,1,1,1,0,
+    0,1,1,1,1,1,1,1,1,0,
+    0,1,1,1,1,1,1,1,1,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+  ];
+
   constructor() {
-    this.grid = new Array(BRICK_ROWS * BRICK_COLS);
+    this.grid = new Array();
     this.brickCounter = 0;    
   }
 
   reset() {
+    this.buildLevel(this.LEVEL1_GRID);
+  }
+
+  buildLevel(levelGrid) {
     this.brickCounter = 0;
-    for (let i=3*BRICK_COLS; i<BRICK_ROWS * BRICK_COLS; i++) {
-      this.grid[i]	= 1;
-      this.brickCounter++;
+    this.grid = levelGrid.slice();
+
+    for (let i=0; i<this.grid.length; i++) {
+      if (this.grid[i] === 1) {
+        this.brickCounter++;
+      }
     }
   }
 
@@ -30,16 +100,25 @@ class BrickLevel {
   }
   
   draw() {
+    let brickIndex = 0;
+    let brickX;
+    let brickY = 0.5*BRICK_H;
+    let brickType;
+    
     for (let i=0;	i<BRICK_ROWS;	i++) {
-      for (let j=0;	j<BRICK_COLS;	j++) {
-        if (this.doesBrickExist(i, j)) {
-          let	brickX = j * BRICK_W;
-          let	brickY = i * BRICK_H + TOP_INFO_HEIGHT;
-  
-          drawImageCenteredAtLocationWithScaling(brick1Pic, brickX + BRICK_W/2,	brickY + BRICK_H/2, BRICK_W, BRICK_H);
-          
+      brickX = 0.5*BRICK_W;
+      for (var j=0;	j<BRICK_COLS;	j++) {
+        brickType = this.grid[brickIndex];
+
+        if (brickType === 1) {
+          drawImageCenteredAtLocationWithScaling(brick1Pic, brickX,	brickY, BRICK_W, BRICK_H);
         }
+
+        brickIndex++;
+        
+        brickX += BRICK_W;
       }
+      brickY += BRICK_H;
     }
   
   }
